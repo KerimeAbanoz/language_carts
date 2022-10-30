e7bf8924805e300ff83d47cd6ba3aad45f6bbf75
kerime abanoz <krmeabnz@gmail.com>
2022-10-30 18:09:36 +0300

    done

--- src/components/Language.jsx
+++ src/components/Language.jsx
@@ -2,26 +2,30 @@ import { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Image from "react-bootstrap/Image";
 const Language = ({ name, options, img }) => {
-  const [showDesc, setShowDesc] = useState(false);
+  const [showImage, setShowImage] = useState(true);
   // console.log(name, img, options);
-  console.log(showDesc)
+  console.log(showImage);
   return (
     <Container
       style={{ background: "peachpuff" }}
-      className="p-4 rounded-2"
-      onClick={()=>setShowDesc(!showDesc)}
+      className="p-4 rounded-2 h-100 lang-card"
+      onClick={() => setShowImage(!showImage)}
+      type="button"
     >
-      <Container>
-        <Image src={img} width="70%"></Image>
-        <h3 className="display-6">{name}</h3>
-      </Container>
-      <ol>
-        {options.map((item)=> {
-          return (
-            <li className="h5 text-start">{item}</li>
-          )
-        })}
-      </ol>
+      {showImage && (
+        <Container>
+          <Image className="lang-logo" src={img} width="70%"></Image>
+          <h3 className="display-6">{name}</h3>
+        </Container>
+      )}
+
+      {!showImage && (
+        <ol className="h-100 d-flex flex-column jusify-content-center">
+          {options.map((item) => {
+            return <li className="h5 text-start">{item}</li>;
+          })}
+        </ol>
+      )}
     </Container>
   );
 };
