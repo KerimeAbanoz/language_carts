44136bcc8bb47cf50ca5ccc6c35c1cda1500e346
kerime abanoz <krmeabnz@gmail.com>
2022-10-30 17:32:03 +0300

    ol

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from "./components/Header";
 import Container from "react-bootstrap/Container";
 function App() {
   return (
-    <Container className="text-center mt-4">
+    <Container className="text-center mt-4 p-4">
       <Header />
       <Card />
     </Container>
